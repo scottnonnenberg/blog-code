ba41635894f46935522bff06c82d6f78e3c3e16c
Scott Nonnenberg <scott@nonnenberg.com>
2016-05-13 10:46:39 -0700

    Async proposal: Clean up style of new auto example

--- a-modest-async-proposal/06. callback_refactored_auto.js
+++ a-modest-async-proposal/06. callback_refactored_auto.js
@@ -3,57 +3,52 @@
 
 var async = require('async');
 var _ = require('lodash');
-var assert = require('assert');
 
 var lib = require('./lib_callbacks');
 
 
 var saveUserNewPostReferences = function(id, cb) {
   async.auto({
+    // getUser and getPosts are run in parallel
     getUser: function(cb) {
       lib.db.getUser(id, cb);
     },
-    getPosts: ['getUser', function(cb) {
+    getPosts: function(cb) {
       lib.db.getPosts(id, cb);
-    }],
+    },
     getAllNewReferences: ['getPosts', function(cb, result) {
-      var posts = result.getPosts,
-          check = lib.twitter.checkForNewReferences.bind(lib.twitter) ;
+      var posts = result.getPosts;
+      var check = lib.twitter.checkForNewReferences.bind(lib.twitter);
+
       async.map(posts, check, cb);
     }],
     saveReferences: ['getAllNewReferences', function(cb, result) {
-      var references = _.flatten(result.getAllNewReferences);
-      async.map(references, lib.db.saveReference.bind(lib.db), cb);
+      var references = result.getAllNewReferences;
+      var save = lib.db.saveReference.bind(lib.db);
+
+      async.map(_.flatten(references), save, cb);
     }],
-    emailUserIf: ['saveReferences', function(cb, result) {
-      var references = result.saveReferences,
-          user = result.getUser;
-          if (references.length > 0) {
-            lib.email.send(user.email, 'You have new mentions!', function(err) {
-              cb(err, true);
-            });
-          } else {
-            return cb(null, false);
-          }
+
+    // we don't actually need the user until this point
+    emailUserIf: ['saveReferences', 'getUser', function(cb, result) {
+      var references = result.saveReferences;
+      var user = result.getUser;
+
+      if (references.length > 0) {
+        return lib.email.send(user.email, 'You have new mentions!', function(err) {
+          cb(err, true);
+        });
+      }
+
+      return cb(null, false);
     }]
   }, cb);
 };
 
-async.series([
-  function testSuccess(cb) {
-    saveUserNewPostReferences(3, function(err, result) {
-      if (err) {
-        return console.log(err.stack);
-      }
-      console.log('newReferences:', result.emailUserIf);
-      cb();
-    });
-  },
-  // function testFailure(callback) {
-  //   saveUserNewPostReferences(25, function(err, result) {
-  //     assert(err);
-  //     assert(!result.emailUserIf);
-  //     cb();
-  //   });
-  // }
-]);
+saveUserNewPostReferences(3, function(err, result) {
+  if (err) {
+    return console.log(err.stack);
+  }
+
+  console.log('newReferences:', result.emailUserIf);
+});
